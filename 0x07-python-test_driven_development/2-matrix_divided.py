d05996aa553bd926852f02911241e1b0ce1fd31e
MutisoMuli <mulimutiso525@gmail.com>
2023-11-29 00:12:55 +0300

    Done

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -5,7 +5,7 @@ This module defines a matrix division function
 
 
 def matrix_divided(matrix, div):
-    """This function devides all elements of a matrix by a given number
+    """This function divides all elements of a matrix by a given number
     Args:
         matrix: A list of lists (matrix)- members can be of type ints or floats
         div: Number to be used for the division (can be a float or an integer)
@@ -15,13 +15,14 @@ def matrix_divided(matrix, div):
         TypeError: If div is not an int or float
         ZeroDivisionError: If div is 0
     Returns:
-        A new matrix which represents the results of the divisions
+        A new matrix which represents the result of the divisions
     """
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
             not all((isinstance(ele, int) or isinstance(ele, float))
-	            for ele in [num for row in matrix for num in row])):
-       raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+                    for ele in [num for row in matrix for num in row])):
+        raise TypeError("matrix must be a matrix (list of lists) of "
+                        "integers/floats")
 
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
@@ -33,18 +34,3 @@ def matrix_divided(matrix, div):
         raise ZeroDivisionError("division by zero")
 
     return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -18,45 +18,44 @@ class TestMaxInteger(unittest.TestCase):
         unordered = [1, 2, 4, 3]
         self.assertEqual(max_integer(unordered), 4)
 
-    def test_max_at_beginning(self):
+    def test_max_at_begginning(self):
         """Test a list with a beginning max value."""
         max_at_beginning = [4, 3, 2, 1]
-	self.assertEqual(max_integer(max_at_beginning), 4)
+        self.assertEqual(max_integer(max_at_beginning), 4)
 
     def test_empty_list(self):
         """Test an empty list."""
         empty = []
-	self.assertEqual(max_integer(empty), None)
+        self.assertEqual(max_integer(empty), None)
 
     def test_one_element_list(self):
         """Test a list with a single element."""
-	one_element = [7]
+        one_element = [7]
         self.assertEqual(max_integer(one_element), 7)
 
     def test_floats(self):
         """Test a list of floats."""
         floats = [1.53, 6.33, -9.123, 15.2, 6.0]
-	self.assertEqual(max_integer(floats), 15.2)
+        self.assertEqual(max_integer(floats), 15.2)
 
     def test_ints_and_floats(self):
         """Test a list of ints and floats."""
         ints_and_floats = [1.53, 15.5, -9, 15, 6]
-	self.assertEqual(max_integer(int_and_floats), 15.5)
+        self.assertEqual(max_integer(ints_and_floats), 15.5)
 
     def test_string(self):
-        """Test a strings"""
-        string = "Mutiso"
+        """Test a string."""
+        string = "Ehoneah"
         self.assertEqual(max_integer(string), 'r')
 
     def test_list_of_strings(self):
         """Test a list of strings."""
-        strings = ["Mutiso", "is", "my", "name"]
-	self.assertEqual(max_integer(string), 'name')
+        strings = ["Ehoneah", "is", "my", "name"]
+        self.assertEqual(max_integer(strings), "name")
 
     def test_empty_string(self):
         """Test an empty string."""
-        self.assertEqual(max_integer(""), 'None')
-
+        self.assertEqual(max_integer([]), None)
 
 if __name__ == '__main__':
     unittest.main()
