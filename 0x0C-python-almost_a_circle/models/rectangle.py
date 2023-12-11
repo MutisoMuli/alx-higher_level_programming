1a6d7d79731aaf04733fbeba4d91b59d58bdcf07
MutisoMuli <mulimutiso525@gmail.com>
2023-12-11 18:14:58 +0300

    Done

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -131,7 +131,8 @@ class Rectangle(Base):
     def __str__(self):
         """Method that override str method
         """
-        return ("[Rectangle] ({}) {}/{}".format(self.id, self.x, self.y, self.width, self.height))
+        return ("[Rectangle] ({}) {}/{}".format(self.id, self.x, self.y,
+                self.width, self.height))
 
     def update(self, *args, **kwargs):
         """Method that changed the order of arguments for rectangle object
