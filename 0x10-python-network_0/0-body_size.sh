7073297797aa39a494b1a8124e681ec79a8b8f4c
MutisoMuli <mulimutiso525@gmail.com>
2024-02-29 23:30:47 +0300

    Done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# Check if URL argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get the size of the body of the response
-body_size=$(curl -sI "$1" | awk '/Content-Length/ {print $2}' | tr -d '\r')
-
-# Display the size in bytes
-echo "$body_size"
+# Get the comtent-lenght of a giving ip address
+curl -sI "$1" | awk '/Content-Length/{print $2}'
